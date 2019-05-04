b2c327835cb6f58c66cb7c0b0b871ea0f12506a1
WrathZA <miller.brettm@gmail.com>
2019-05-04 16:21:01 +0200

    did some stuff after gym

--- presentation/index.js
+++ presentation/index.js
@@ -32,6 +32,8 @@ const images = {
   son: require("../assets/sonof.png"),
   ctjs: require("../assets/ctjs.png"),
   cptmsug: require("../assets/ctmsug.jpg"),
+  docker: require("../assets/docker_400x400.png"),
+  dockervm: require("../assets/docker-vm-container.png")
 };
 
 preloader(images);
@@ -66,6 +68,76 @@ export default class Presentation extends React.Component {
             HELLO!
           </Heading>
           <Image margin="50px auto" src={images.son.replace("/", "")} margin="0px auto 50px" height="700px" />
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading margin={"0px auto 20px"} size={2} fill caps lineHeight={1} textColor="tertiary">
+            What are we going to talk about tonight?
+          </Heading>
+          <div style={{paddingLeft: "300px" }}>
+            <List >
+              <Appear>
+                <ListItem textSize={56}>What is Docker?</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem textSize={56}>What is Container?</ListItem>
+              </Appear> 
+              <Appear> 
+                <ListItem textSize={56}>dotnet core versus .NET Framework </ListItem>
+              </Appear>  
+              <Appear>  
+                <ListItem textSize={56}>Why Docker?</ListItem>
+              </Appear>  
+              <Appear>
+                <ListItem textSize={56}>Microservice DEMO</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem textSize={56}>Cool Stuff (Swagger, Ocelot, XUnit)</ListItem>
+              </Appear>
+            </List>
+          </div>
+          <Notes>
+            <ul>
+              <li> </li>
+            </ul>
+          </Notes>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading margin={"0px auto 20px"} size={1} fill caps lineHeight={1} textColor="tertiary">
+            Docker?
+          </Heading>
+          <div style={{ width: '40%', float: 'left' }}>
+            <Image margin="50px auto" src={images.docker.replace("/", "")} margin="0px auto 50px" height="500px" />
+          </div>
+          <div style={{ paddingLeft: "25px", width: '60%', float: 'right' }}>
+            <List >
+              <Appear>
+                <ListItem padding={"10px 0"} textSize={56}>Docker is a computer program that performs operating-system-level virtualization.</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem padding={"10px 0"} textSize={56}>HUH?!</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem padding={"10px 0"} textSize={56}>Docker engine, converts images in containers by running them</ListItem>
+              </Appear>
+              <Appear>
+                <ListItem padding={"10px 0"} textSize={56}>Images come from repositories. </ListItem>
+              </Appear>
+            </List>
+          </div>
+          <Notes>
+            <ul>
+              <li>original author of Turbo Pascal </li>
+            </ul>
+          </Notes>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fill caps lineHeight={1} textColor="secondary">
+            VM's Versus Containers
+          </Heading>
+          <Image margin="50px auto" src={images.dockervm.replace("/", "")} margin="0px auto 50px" height="700px" />
         </Slide>
         
         <Slide transition={["zoom"]} bgColor="primary">
